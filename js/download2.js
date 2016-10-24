baf4ef9aada730daba5c78b7404eb75f38bcba93
Wansit Siriluck <hatyai_ratree@hotmail.com>
2016-10-25 00:52:00 +0700

    -

--- js/download2.js
+++ js/download2.js
@@ -1,21 +1,22 @@
 var folderName = 'coeispacs2016';
-var fileName;
+var fileName = "ISPACS2016-Proceeding.pdf";
 var URL_proceeding = "http://ispacs2016.psu.ac.th/pdf/ISPACS2016-Proceeding.pdf";
 var statusDom;
+var filesys;
 function downloadProceeding(){
 	downloadFile("http://ispacs2016.psu.ac.th/pdf/ISPACS2016-Proceeding.pdf");
 }
 
 function downloadFile(URL) {
     //step to request a file system
-    var filesys;
+    
 	var download_link = encodeURI(URL);
 	statusDom = document.querySelector('#status');
 	window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, fileSystemSuccess, fileSystemFail);
 	
     function fileSystemSuccess(fileSystem) {
         
-        fileName = download_link.substr(download_link.lastIndexOf('/') + 1); //Get filename of URL
+        //fileName = download_link.substr(download_link.lastIndexOf('/') + 1); //Get filename of URL
         
 		var directoryEntry = fileSystem.root; // to get root path of directory
         filesys = fileSystem;
@@ -30,7 +31,7 @@ function downloadFile(URL) {
         // Directory created successfuly
 		var rootdir = filesys.root;
         //var fp = rootdir.toNativeURL(); // Returns Fullpath of local directory
-		var fp = filesys.root.toNativeURL();
+		var fp = filesys.root.toURL();
         fp = fp + folderName + "/" + fileName; // fullpath and name of the file which we want to give
         // download function call
 		//alert("SAVE to " + fp);
@@ -66,12 +67,38 @@ function filetransfer(download_link, fp) {
 	};	
     fileTransfer.download(download_link, fp,
         function(entry) {
-            alert("download complete: " + entry.fullPath);
-			statusDom.innerHTML = entry.fullPath();
+            statusDom.innerHTML = entry.fullPath;
+			alert("download complete: " + entry.fullPath);
+			checkIfFileExists(entry.fullPath);
+			checkIfFileExists(entry.toURL());			
         },
         function(error) {
             //Download abort errors or download failed errors
             alert("download error source " + error.source);
         }
     );
+}
+
+function isExist(){
+	
+			checkIfFileExists(filesys.root.fullPath);
+			checkIfFileExists(filesys.root.toURL());	
+}
+function openFile(){
+	cordova.InAppBrowser.open(filesys.root.toURL(), "_self", "location=no,hidden=yes");
+}
+
+function checkIfFileExists(path){
+    window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(fileSystem){
+        fileSystem.root.getFile(path, { create: false }, fileExists, fileDoesNotExist);
+    }, getFSFail); //of requestFileSystem
+}
+function fileExists(fileEntry){
+    alert("File " + fileEntry.fullPath + " exists!");
+}
+function fileDoesNotExist(){
+    alert("file does not exist");
+}
+function getFSFail(evt) {
+    console.log(evt.target.error.code);
 }
